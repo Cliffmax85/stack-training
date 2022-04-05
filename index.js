49f815a57b1115a8f1f682ffe2900d00fd651a72
cliffmax85 <cliffmax85@gmail.com>
2022-04-05 11:24:33 -0700

    done with stacks and queus

--- index.js
+++ index.js
@@ -32,3 +32,47 @@ console.log(stack);
 stack.peek();
 
 console.log('peek', stack.peek());
+
+// QUEUE
+
+class Queue {
+
+  constructor() 
+  {
+    this.queue = [];
+  }
+  
+  enqueue(item) {
+    return this.queue.unshift(item);
+  }
+  
+  dequeue() {
+    return this.queue.pop();
+  }
+  
+  hasNext() {
+    // (this.queue.length !== 0) ? true : false;
+    if (this.queue.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+  
+  
+  
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log('queue', queue);
+console.log(queue.dequeue()); // 'fox'
+console.log('hasNext?', queue.hasNext()); // true
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue('llama');
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // false
+console.log(queue.dequeue()); // null
