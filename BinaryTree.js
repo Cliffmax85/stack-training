2f8e42fd83c9f0d68ef1dbecfe13ff563eea4f95
cliffmax85 <cliffmax85@gmail.com>
2022-04-05 18:40:18 -0700

    finished binaryTree

--- BinaryTree.js
+++ BinaryTree.js
@@ -6,20 +6,23 @@ class BinaryTreeNode {
   }
   
   add(node) {
-    if (!this.left && !this.right) {
-      if (this.value < node) {
-        this.right = node;
-      }
-      elseif (this.value > node) {
-          this.left = node;
-        }
-      }
-    } else {
-        if (this.value < node) {
-            return this.right.add(node);
-        } elseif (this.value > node) {
-            return this.left.add(node);
-        }
+    if(node.value === this.value) {
+      //edge case if duplicate value just return cuz already in tree
+      return;
+    }
+    // if node value is less than this valu go left
+    if(node.value < this.value) {
+      // if this .left doesnt exist make this.left node
+      if(!this.left) this.left = node;
+      // else recursively run add on this.left passing in node
+      else this.left.add(node);
+    }
+    //if node value is greater than this.value go right
+    else {
+      // if this.right doesnt exist make this.right equal to node passed in
+      if(!this.right) this.right = node;
+      //if there is a this.right, run add on that this.right passing in node
+      else this.right.add(node);
     }
   }
 }
@@ -34,3 +37,4 @@ const D = new BinaryTreeNode('D');
 B.add(A);
 B.add(D);
 B.add(C);
+console.log(B);
